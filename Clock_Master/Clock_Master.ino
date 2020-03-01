3d5acaa7d3cc69cd33c134017143e81a42430d81
HyperbolicStudios <marke9135@sd61learn.ca>
2020-02-29 17:26:04 -0800

    Initial v2 code

--- Clock_Master/Clock_Master.ino
+++ Clock_Master/Clock_Master.ino
@@ -7,6 +7,7 @@ int x;
 int value;
 int a;
 String b;
+int currentDigit;
 // Digit Mapping Sets
 
 int mappingSets[10][13] = {
@@ -79,91 +80,80 @@ int mappingSets[10][13] = {
 void setup() {
   // put your setup code here, to run once:
 
-  Wire.begin();
+  Wire.begin(1); //Join bus as device #1
+  Wire.onReceive(receiveEvent); // register event
   //Serial.begin(9600);
-  setTime(23, 51, 40, 1, 1, 2001);
+  setTime(22, 53, 40, 1, 1, 2001);
 
   for (int thisPin = 1; thisPin < 14; thisPin++) { //Set pins to be output
   pinMode(thisPin, OUTPUT);
   }
   pinMode(A1, OUTPUT);
- 
-  Wire.beginTransmission(1); // transmit to device #1
-  Wire.write(1);        // send one byte - int 1, as a ping to device
-  Wire.endTransmission();    // stop transmitting
-
 }
-
 void loop() {
+  //nothing here
+  }
+  
+
+
+void receiveEvent(int x) {
   // put your main code here, to run repeatedly:
   time_t t = now();   //Get current time
+  x = Wire.read();
   /*Serial.print(hour());
   Serial.print(" ");
   Serial.print(minute());
   Serial.print(" ");
   Serial.println(second()); */  
-  switchDigit(1);
+  if (x==1) {
   //Write the Hour
-  value = hour();   //Put Hour into a variable
-  if((value >= 22 and minute() > 30) or value > 22 or value < 5 or (value <= 5 and minute() < 45)) {
-      Serial.println(value);
-      for (int thisPin = 1; thisPin < 14; thisPin++) { //Set pins to be output
-        digitalWrite(thisPin, LOW);
-  }}
-  else {
-      
-  
+    value = hour();
     
-    if (value > 9) {
-      b = (String(value))[0];
-      writeDigit(b.toInt()); //Write the first digit
-      delay(timeDelay);
-      
-      switchDigit(2);
-      
-      b = (String(value))[1];
-      writeDigit(b.toInt()); //Write the first digit
-      delay(timeDelay);
-    }
-  
-    else {
-      writeDigit(0); //first digit will be a zero
-      delay(timeDelay);
-      switchDigit(2);
-      writeDigit(value); //write the "Value" digit
-      delay(timeDelay);
-    }
-    
-    switchDigit(3);
-  
-    //Write the minute
-  
-  
-    value = minute();   //Put Minute into a variable
-    if (value > 9) {
-      
-      b = (String(value))[0];
-      writeDigit(b.toInt()); //Write the first digit
-      delay(timeDelay);
-      
-      switchDigit(4);
-  
-      b = (String(value))[1];
-      writeDigit(b.toInt());
-      delay(timeDelay);
-    }
-  
-    else {
+    if (value < 10) {
       writeDigit(0);
-      delay(timeDelay);
-      switchDigit(4);
+      b = (String(value))[0];
+     
+    }
+    else {
+       b = (String(value))[0];
+       writeDigit(b.toInt());
+      b = (String(value))[1];
       
-      writeDigit(value); //write the digit
-      delay(timeDelay);
     }
     
-  }   
-} //End Main Loop
+  }
+
+    
+
+  else if (x==2) {
+     writeDigit(b.toInt());
+  }
+
+  else if (x == 3) {
+    value = minute();
+    
+    if (value < 10) {
+      writeDigit(0);
+      b = (String(value))[0];
+     
+    }
+    else {
+      b = (String(value))[0];
+      writeDigit(b.toInt());
+      b = String(value)[1];
+      
+    }
+   
+    
+  }
+
+  else if (x ==4 ) {
+    writeDigit(b.toInt());
+  }
+  
+
+  
+}
 
 
 
@@ -191,14 +181,3 @@ void writeDigit(int number) {
   
 }
 */
-
-
-
-void switchDigit(int x) {
-
-  Wire.beginTransmission(1); // transmit to device #1
- 
-  Wire.write(x);        // send one byte - int 1, as a ping to device
-  Wire.endTransmission();    // stop transmitting
-
-}
